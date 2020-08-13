8bb7dbac807b38a2f2d92b15d3a49998ed808d73
Cupcake-master <Bulat-cloud-1@yandex.ru>
2020-08-13 12:38:46 +0300

    Adding lesson_3.py

---
+++ lessons/lesson_2.py
@@ -0,0 +1,5 @@
+a, b = 4, 5.6
+s = 'Hello'
+check = True
+print(type(a), type(b))
+print(type(s), type(check))
---
+++ lessons/lesson_3.py
@@ -0,0 +1,5 @@
+print('Периметр:', float(input('Enter a: ')) +
+      float(input('Enter b: ')) +
+      float(input('Enter c: ')))
+a, b, c = map(float, input().split())
+print(a, b, c)
